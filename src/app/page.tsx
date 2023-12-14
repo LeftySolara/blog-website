dbfb966127e5b508db679b6ac86ae8e5dda57963
Julianne Adams <julianne@julianneadams.dev>
2023-12-14 06:35:44 -0500

    Add: eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-const Home = () => {
-  return (
-    <main>
-      <h1>Hello World!</h1>
-    </main>
-  );
-};
+const Home = () => (
+  <main>
+    <h1>Hello World!</h1>
+  </main>
+);
 
 export default Home;
