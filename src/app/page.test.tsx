c181c50333db1cc445a7cde7571173704eb430a0
Julianne Adams <julianne@julianneadams.dev>
2023-12-14 06:56:21 -0500

    Add: vitest config

---
+++ src/app/page.test.tsx
@@ -0,0 +1,13 @@
+import { expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+it("Home", () => {
+  render(<Home />);
+
+  const heading = screen.getByRole("heading", {
+    level: 1,
+    name: "Hello World!",
+  });
+  expect(heading).toBeDefined();
+});
