ed8d76fed4a081d5208c097f65e5f30d6a4d1243
Julianne Adams <julianne@julianneadams.dev>
2023-12-20 07:12:06 -0500

    Add: posts page

---
+++ src/app/(routes)/posts/page.tsx
@@ -0,0 +1,36 @@
+import { PostInfoArray, PostList } from "@/app/_components/PostList/PostList";
+import styles from "./page.module.scss";
+
+const PostsPage = () => {
+  const posts: PostInfoArray = [
+    {
+      dateString: "2021-09-01T15:21:39.862Z",
+      title: "An Example Post",
+      href: "#",
+    },
+    {
+      dateString: "2021-09-01T15:21:39.862Z",
+      title: "An Example Post",
+      href: "#",
+    },
+    {
+      dateString: "2021-09-01T15:21:39.862Z",
+      title: "An Example Post",
+      href: "#",
+    },
+    {
+      dateString: "2021-09-01T15:21:39.862Z",
+      title: "An Example Post",
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className={styles["posts-page"]}>
+      <h2>All Posts</h2>
+      <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default PostsPage;
